3f1ba0fb6e8e59a9144f689e3ef7a02c0ed9ec56
Andreas <andreaspapas872@gmail.com>
2026-01-08 13:14:56 -0500

    adding videos function

--- sheets_updating/yt_read.py
+++ sheets_updating/yt_read.py
@@ -1,5 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import sqlite3
+from datetime import date
+
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -7,14 +10,34 @@ SCOPES = [
 ]
 
 creds = Credentials.from_service_account_file(
-    r'C:/Users/andre/YT_View_Tracker/sheets_updating/google-creds.json',
+    'sheets_updating/google-creds.json',
     scopes=SCOPES
 )
 
 client = gspread.authorize(creds)
-
 sheet = client.open("YouTube_View_Tracker")
 videos_tab = sheet.worksheet("Input_Videos")
-
 rows = videos_tab.get_all_records()
-print(rows)
+
+conn = sqlite3.connect('view_tracker.db')
+c = conn.cursor()
+
+
+c.execute("SELECT video_name FROM videos_info")
+current_names = c.fetchall()
+names_list = [x for tup in current_names for x in tup]
+
+#adding new vidoes to db
+videos_to_add = []
+for row in rows:
+    if row['Video Name'] not in names_list:
+        video = tuple([row['Video Name'], row['Video Link'], date.today().strftime("%Y-%m-%d")])
+        videos_to_add.append(video)
+
+c.executemany("INSERT INTO videos_info VALUES(?,?,?)", videos_tab)
+
+
+
+conn.commit()
+conn.close()
+    
--- sql/main.py
+++ sql/main.py
@@ -1,11 +1,22 @@
 import sqlite3
 
 
-conn = sqlite3.connect('video.db')
+conn = sqlite3.connect('view_tracker.db')
 
 c = conn.cursor()
 
+c.execute(""" CREATE TABLE videos_info (
+          video_name TEXT,
+          video_link TEXT,
+          date_added TEXT
+          )""")
 
+c.execute(""" CREATE TABLE views (
+          date TEXT,
+          name TEXT,
+          link TEXT,
+          views TEXT
+          )""")
 
 conn.commit()
 
