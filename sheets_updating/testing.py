cf81b0b40c048c8af082f5a24c9c4c7d28309480
Andreas <andreaspapas872@gmail.com>
2026-01-08 19:20:24 -0500

    yt apiand writing to sheets

---
+++ sheets_updating/testing.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import sqlite3 
+
+
+conn = sqlite3.connect('view_tracker.db')
+c = conn.cursor()
+
+c.execute("SELECT * FROM videos_info")
+
+items = c.fetchall()
+for item in items:
+    print(item)
+print("====================================================================")
+c.execute("SELECT * FROM views")
+
+items = c.fetchall()
+for item in items:
+    print(item)
+
+conn.commit()
+conn.close()
--- sheets_updating/yt_read.py
+++ sheets_updating/yt_read.py
@@ -18,26 +18,49 @@ client = gspread.authorize(creds)
 sheet = client.open("YouTube_View_Tracker")
 videos_tab = sheet.worksheet("Input_Videos")
 rows = videos_tab.get_all_records()
+input_videos = [row['Video Name'] for row in rows]
 
 conn = sqlite3.connect('view_tracker.db')
 c = conn.cursor()
 
-
 c.execute("SELECT video_name FROM videos_info")
 current_names = c.fetchall()
 names_list = [x for tup in current_names for x in tup]
+new_vids = []
 
 #adding new vidoes to db
 videos_to_add = []
 for row in rows:
     if row['Video Name'] not in names_list:
+        new_vids.append(row['Video Name'])
         video = tuple([row['Video Name'], row['Video Link'], date.today().strftime("%Y-%m-%d")])
         videos_to_add.append(video)
 
-c.executemany("INSERT INTO videos_info VALUES(?,?,?)", videos_tab)
+c.executemany("INSERT INTO videos_info VALUES(?,?,?)", videos_to_add)
 
-
+#deleting videos from db
+for name in names_list:
+    if name not in input_videos:
+        c.execute(f"DELETE from videos_info WHERE video_name = ?",(name,))
 
 conn.commit()
 conn.close()
+
+tab = sheet.worksheet("Input_Videos")
+
+values = tab.get_all_values()
+
+updates = []
+
+for i, row in enumerate(values[1:], start=2):
+    if row[0] in new_vids:
+        updates.append({
+            "range": f"C{i}:D{i}",
+            "values": [['Active', str(date.today())]]
+        })
+
+if updates:
+    tab.batch_update(updates)
+
+
     
